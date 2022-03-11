c73d52ff7bb5173ec07597f1e1e0998e568d2666
Ricardo Rocha <rochar@gmail.com>
2022-03-11 14:24:34 +0000

    create solution and projects

---
+++ SimpleCudaNet/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"SimpleCudaNet")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"FARO")];
+[assembly:AssemblyProductAttribute(L"SimpleCudaNet")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) FARO 2022")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute(L"1.0.*")];
+
+[assembly:ComVisible(false)];
